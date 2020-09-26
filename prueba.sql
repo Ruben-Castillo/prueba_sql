ffade24f5ac02a957474949a05aae7fe213dfdd3
Rubén Ignacio Castillo Díaz <ruben.cas@outlook.com>
2020-09-26 15:31:29 -0300

    diagram added

--- prueba.sql
+++ prueba.sql
@@ -4,7 +4,6 @@ CREATE TABLE dates (
     dates DATE UNIQUE NOT NULL,
     PRIMARY KEY(dates)
 );
-
  CREATE TABLE clientes (
      id SERIAL,
      nombre VARCHAR (50) NOT NULL,
@@ -12,14 +11,12 @@ CREATE TABLE dates (
      direccion VARCHAR NOT NULL,
      PRIMARY KEY(id)
  );
-
  CREATE TABLE categorias (
      id SERIAL,
      nombre VARCHAR(50) UNIQUE NOT NULL,
      descripcion VARCHAR,
      PRIMARY KEY (id)
  );
- 
  CREATE TABLE productos (
      id SERIAL,
      nombre VARCHAR(50) UNIQUE NOT NULL,
@@ -48,19 +45,16 @@ CREATE TABLE dates (
      FOREIGN KEY (factura_id) REFERENCES facturas(id),
      FOREIGN KEY (producto_id) REFERENCES productos(id)
  );
-
+ --Por la cantidad de INSERT me pareció más eficiente importar desde archivos CSV
 \copy dates(dates) FROM '~/Documentos/DESAFIO_LATAM/Modulo_3/prueba/dates.csv' CSV;
 \copy clientes(nombre,rut,direccion) FROM '~/Documentos/DESAFIO_LATAM/Modulo_3/prueba/clientes.csv' CSV HEADER;
 \copy categorias(nombre,descripcion) FROM '~/Documentos/DESAFIO_LATAM/Modulo_3/prueba/categorias.csv' CSV HEADER;
 \copy productos(nombre,precio_unitario,descripcion,categoria_id) FROM '~/Documentos/DESAFIO_LATAM/Modulo_3/prueba/productos.csv' CSV HEADER;
 \copy facturas(fecha,cliente_id,subtotal) FROM '~/Documentos/DESAFIO_LATAM/Modulo_3/prueba/facturas.csv' CSV HEADER;
 \copy facturas_productos(factura_id,producto_id,cantidad,total_producto) FROM '~/Documentos/DESAFIO_LATAM/Modulo_3/prueba/facturas_productos.csv' CSV HEADER;
-
 --¿Que cliente realizó la compra más cara? (Si más de un cliente comparten la compra más cara, se mostrarán ambos clientes)
 SELECT DISTINCT (nombre) FROM clientes INNER JOIN facturas ON clientes.id=facturas.cliente_id WHERE subtotal IN (SELECT MAX (subtotal) FROM facturas);
-
 --¿Que cliente pagó sobre 100 de monto? (si en alguna factura el cliente pagó más de 100, se considera en la selección. Si el cliente ha pagado varias facturas que en total suman más de 100, pero el valor de cada factura es menos que 100, no se considera)
 SELECT DISTINCT(nombre) FROM clientes INNER JOIN facturas ON clientes.id=facturas.cliente_id WHERE subtotal>100;
-
 --¿Cuantos clientes han comprado el producto 6?
 SELECT COUNT (distinct cliente_id) FROM facturas INNER JOIN facturas_productos ON facturas.id=facturas_productos.factura_id WHERE producto_id=6;
